d41bd9f03e341df86bd9cc9e35243fbbcb2c14d0
johnb <johnb@Js-MacBook-Air.local>
2016-03-05 12:27:41 -0500

    trying to figure out xhr header shit.

--- rails/spiralizer/app/assets/javascripts/login.js
+++ rails/spiralizer/app/assets/javascripts/login.js
@@ -1,10 +1,30 @@
 $(document).on('ready page:load',function(){
    $("#new_user").on("ajax:success", function(e, data, status, xhr){
 	   $("#user_area").html('<li><a rel="nofollow" data-method="delete" href="/users/sign_out">Logout</a></li>');
-	   console.log("devise hitting back good")
+	    console.log("devise hitting back good")
+	   var csrf_param = xhr.getResponseHeader('X-Csrf-Param');
+	   var csrf_token = xhr.getResponseHeader('X-Csrf-Token');
+	   console.log(xhr);
+	   console.log("csrf_param: "+csrf_param);
+	   console.log("csrf_token: "+csrf_token);
+	   var headers = xhr.getAllResponseHeaders().toLowerCase();
+	   console.log(headers);
+	  
+	   
+	   if (csrf_param) {
+	     $('meta[name="csrf-param"]').attr('content', csrf_param);
+	   }
+	   if (csrf_token) {
+	     $('meta[name="csrf-token"]').attr('content', csrf_token);
+	   }
+	   
    }).on("ajax:error", function (e, xhr, status, error){
      console.log("devise hitting back bad")
-   })
-   console.log("INSIDE #new_user");  
+   }).on("ajax:complete", function(e,xhr,status){
+	   console.log("in complete");
+	   
+   });
+   
 })
+
 
--- rails/spiralizer/config/routes.rb
+++ rails/spiralizer/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  devise_for :users
+  devise_for :users, :controllers => {sessions: 'sessions'}
   
   get 'user/profile' => 'user#profile'
   get 'user/edit'    => 'user#edit'
